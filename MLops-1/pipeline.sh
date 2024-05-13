3206a38b19bfe07088eef1cfcc8be250d289e03a
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-05-13 11:42:52 +0300

    Update pipeline.sh

--- MLops-1/pipeline.sh
+++ MLops-1/pipeline.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-python3 data_creation.py & python3 data_creation.py & python3 model_preparation.py & python3 model_testing.py
+python3 data_creation.py 
+python3 data_creation.py 
+python3 model_preparation.py 
+python3 model_testing.py
 
 
 
